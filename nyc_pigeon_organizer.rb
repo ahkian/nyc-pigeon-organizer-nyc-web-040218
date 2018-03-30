5073ee0d7d4d5942ca4a27ed2031ad0b784e20e3
Daniel Brazel <dbrazel4@gmail.com>
2018-03-30 16:33:27 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,9 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  
-  data.each do |el1, el2|
-    binding.pry 
+  pigeon_list = Hash.new
+  data.each do |category, sub_category|
+    sub_category.each do |el1, el2|
+      binding.pry
+    end 
   end
 end
