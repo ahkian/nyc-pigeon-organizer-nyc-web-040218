d180d3c0385e6cf1ace533cebdd753cfaace9882
Daniel Brazel <dbrazel4@gmail.com>
2018-03-30 16:39:33 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ def nyc_pigeon_organizer(data)
   pigeon_list = Hash.new
   pigeon_names = []
   data.each do |category, sub_category|
-    sub_category.each do |array_name, |
-      el2.each do |name|
+    sub_category.each do |array_name, names|
+      names.each do |name|
         pigeon_names << name
       end
     end 
